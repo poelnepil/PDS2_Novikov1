fe7ab33a859735f479608ab4c9b7295d1f0a725a
Ivan Novikov <vanzo3039@gmail.com>
2022-11-14 11:46:29 +0200

    commit11.1c

---
+++ lesson11/task11.1/client11.1.py
@@ -0,0 +1,18 @@
+# '''client'''
+import socket
+
+sock = socket.socket()
+
+name = input('enter a name: ')
+sock.connect(('localhost', 8000))
+sock.send(name.encode())
+server = sock.recv(1024)
+server = server.decode()
+print(server, 'connected')
+
+while True:
+    message = sock.recv(1024)
+    message = message.decode()
+    print(server, ':', message)
+    message = input('me: ')
+    sock.send(message.encode())
